57b141470293c21ab0c0bd9d4382171ec678c475
Ayala-Yagelnik <a83245064@gmail.com>
2025-03-06 18:48:19 +0200

    refactor profile component; enhance form validation and styling; load user profile on init

--- online-courses/src/app/components/profile/profile.component.ts
+++ online-courses/src/app/components/profile/profile.component.ts
@@ -1,38 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { UserService } from '../../services/user.service';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from '../../services/auth.service';
+import { UserService } from '../../services/user.service';
+import { firstValueFrom } from 'rxjs';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
+import { emptyUser, User } from '../../models/user.model';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, ReactiveFormsModule , HttpClientModule]
+  imports: [MatSelectModule, MatIconModule , MatFormFieldModule, MatInputModule, MatButtonModule, ReactiveFormsModule , HttpClientModule]
 })
-export class ProfileComponent {
-  profileForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private userService: UserService) {
-    this.profileForm = this.fb.group({
-      name: ['', [Validators.required]],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]],
-      role: ['', [Validators.required]]
+export class ProfileComponent implements OnInit{
+  profileForm: FormGroup;
+  hidePassword = true; 
+  user: User = emptyUser;
+
+  constructor(private fb: FormBuilder, private router:Router, private authService: AuthService, private userService: UserService) {
+    console.log('initiate profile form');
+    this.profileForm =this.fb.group({
+      name: ['', Validators.required],
+      role: ['', Validators.required]
     });
   }
 
-  onSubmit() {
+  ngOnInit(): void {
+    const token = this.authService.getToken();
+    this.loadUserProfile(token);
+  }
+  
+  async loadUserProfile(token: string): Promise<void> {
+    try {
+      this.user = await firstValueFrom(this.userService.getUserById(this.authService.getUser().userId, token));
+      this.profileForm.setValue({
+        name: this.user.name,
+        role: this.user.role
+      });
+    } catch (error) {
+      console.error('Error fetching user:', error);
+    }
+  }
+
+  async onSubmit(): Promise<void> {
+    const token = this.authService.getToken();
+
     if (this.profileForm.valid) {
-      this.userService.updateUser(this.profileForm.value.id, this.profileForm.value, 'your_token_here').subscribe(() => {
-        // Handle successful profile update
-      }, error => {
+      try {
+        await firstValueFrom(this.userService.updateUser(this.user?.id, {...this.user, ...this.profileForm.value}, token));
+        this.router.navigate(['/']);
+      } catch (error) {
         // Handle profile update error
         console.error(error);
-      });
+      }
     }
   }
 }
